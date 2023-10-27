5f026031a1b37c068ab45749a57413496d9bf502
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-10-27 23:56:59 +0300

    Initial Commit

---
+++ JS Front-End/JS Syntax Fundamentals/Exercise/RoadRadar.js
@@ -0,0 +1,35 @@
+function print(speed, area) {
+  let limit = 0;
+  switch (area) {
+    case "motorway":
+      limit = 130;
+      break;
+    case "interstate":
+      limit = 90;
+      break;
+    case "city":
+      limit = 50;
+      break;
+    case "residential":
+      limit = 20;
+      break;
+    default:
+      console.log("Error!");
+  }
+  if (speed <= limit) {
+    console.log(`Driving ${speed} km/h in a ${limit} zone`);
+  } else {
+    let status = "";
+    let difference = speed - limit;
+    if (difference <= 20) {
+      status = "speeding";
+    } else if (difference <= 40) {
+      status = "excessive speeding";
+    } else {
+      status = "reckless driving";
+    }
+    console.log(
+      `The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`
+    );
+  }
+}
