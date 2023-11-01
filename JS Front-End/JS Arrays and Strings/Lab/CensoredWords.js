3e49a7048bb4a4ed611f9c0a0be9cb4adf74f60d
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-11-01 23:24:45 +0200

    Initial Commit

--- JS Front-End/JS Arrays and Strings/Lab/CensoredWords.js
+++ JS Front-End/JS Arrays and Strings/Lab/CensoredWords.js
@@ -1,6 +1,12 @@
+// solution 1
 function censore(text, wordToReplace) {
     console.log(text.replace(new RegExp(wordToReplace, 'g'), "*".repeat(wordToReplace.length)));
-}
+} 
 
-censore('A small sentence with some words', 'small');
-censore('Find the hidden word', 'hidden');
+// solution 2
+function censore(text, wordToReplace) {
+    while(text.includes(wordToReplace)) {
+        text = text.replace(wordToReplace, "*".repeat(wordToReplace.length));
+    }
+    console.log(text);
+}
