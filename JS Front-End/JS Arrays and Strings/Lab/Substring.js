b5ba84dfeba868cb9a1e68aa36007053df78be0f
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-11-01 23:16:28 +0200

    Initial Commit

---
+++ JS Front-End/JS Arrays and Strings/Lab/Substring.js
@@ -0,0 +1,7 @@
+function substr(str, startIndex, countOfCharactersToRemove) {
+    console.log(str.substring(startIndex, startIndex + countOfCharactersToRemove));
+}
+
+substr("ASentence", 1, 8);
+substr("SkipWord", 4, 7);
+
