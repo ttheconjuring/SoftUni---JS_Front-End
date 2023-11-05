24a0c732e7ec93b897d83cced6a72b5295c89503
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-11-05 10:13:22 +0200

    Initial Commit

---
+++ JS Front-End/JS Arrays and Strings/Exercise/SortingNumbers.js
@@ -0,0 +1,11 @@
+function specificSort(array) {
+  array.sort((a, b) => {
+    return a - b;
+  });
+  let newArray = [];
+  while(array.length != 0) {
+    newArray.push(array.shift());
+    newArray.push(array.pop());
+  }
+  return newArray;
+}
