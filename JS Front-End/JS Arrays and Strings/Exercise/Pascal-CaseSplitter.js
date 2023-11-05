4c76ba66792799ab299832583fc1ecbc00a72a2d
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-11-05 11:36:43 +0200

    Initial Commit

--- JS Front-End/JS Arrays and Strings/Exercise/Pascal-CaseSplitter.js
+++
@@ -1,8 +0,0 @@
-function splitWords(text) {
-    let matches = text.match(/[A-Z][a-z]*/g);
-    console.log(matches.join(', '));
-}
-
-splitWords('SplitMeIfYouCanHaHaYouCantOrYouCan');
-splitWords('HoldTheDoor');
-splitWords('ThisIsSoAnnoyingToDo');
--- JS Front-End/JS Arrays and Strings/Exercise/PrintEveryNthElementFromAnArray.js
+++
@@ -1,9 +0,0 @@
-function print(array, n) {
-    let newArray = [];
-    const length = array.length;
-    for (let i = 0; i < length; i+= n) {
-        newArray.push(array[i]);
-    }
-    return newArray;
-}
-
