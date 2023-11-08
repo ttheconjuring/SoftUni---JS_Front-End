2891b7ceb43d5a3579b5153533c935f0627e09d7
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-11-08 23:11:55 +0200

    Initial Commit

---
+++ JS Front-End/Functions and Statements/Lab/FormatGrade.js
@@ -0,0 +1,17 @@
+function checkGrade(grade) {
+  if (grade < 3) {
+    console.log(`Fail (2)`);
+  } else if (grade < 3.5) {
+    console.log(`Poor (${grade.toFixed(2)})`);
+  } else if (grade < 4.5) {
+    console.log(`Good (${grade.toFixed(2)})`);
+  } else if (grade < 5.5) {
+    console.log(`Very good (${grade.toFixed(2)})`);
+  } else {
+    console.log(`Excellent (${grade.toFixed(2)})`);
+  }
+}
+
+checkGrade(3.33);
+checkGrade(4.50);
+checkGrade(2.99);
