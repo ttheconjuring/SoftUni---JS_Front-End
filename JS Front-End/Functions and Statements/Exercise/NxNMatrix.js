92bcd7f589379403cfb8225a1212e994db20b2ef
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2023-11-10 14:24:44 +0200

    Initial Commit

---
+++ JS Front-End/Functions and Statements/Exercise/NxNMatrix.js
@@ -0,0 +1,9 @@
+function printMatrix(n) {
+  for (let i = 0; i < n; i++) {
+    let row = [];
+    for (let j = 0; j < n; j++) {
+        row.push(n);
+    }
+    console.log(row.join(' '));
+  }
+}
